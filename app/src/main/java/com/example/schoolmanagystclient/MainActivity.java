20add6755c79f9a3b161e39626e28c081b56c1fc
Tom <tom-vautray@hotmail.fr>
2021-04-09 16:34:01 +0200

    implemented OnClickListener to MainActivity

--- app/src/main/java/com/example/schoolmanagystclient/MainActivity.java
+++ app/src/main/java/com/example/schoolmanagystclient/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.schoolmanagystclient;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.button.MaterialButton;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setPromotionsButton((MaterialButton)findViewById(R.id.promotionsButton));
+        setStudentsButton((MaterialButton)findViewById(R.id.studentsButton));
+        setPromotionsListView((ListView)findViewById(R.id.promotionsListView));
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+
     }
 }
