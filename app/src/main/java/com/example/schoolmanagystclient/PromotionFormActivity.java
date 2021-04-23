1ed6cbba355778b95e5889fb49c78ecffbf1bd19
Tom <tom-vautray@hotmail.fr>
2021-04-23 11:03:34 +0200

    Added form fields set/getters to PromotionFormActivity

--- app/src/main/java/com/example/schoolmanagystclient/PromotionFormActivity.java
+++ app/src/main/java/com/example/schoolmanagystclient/PromotionFormActivity.java
@@ -1,16 +1,54 @@
 package com.example.schoolmanagystclient;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.button.MaterialButton;
+
 public class PromotionFormActivity extends AppCompatActivity
 {
+    private EditText _promotionAcronymEditText;
+    private EditText _promotionEntitledEditText;
+
+    private MaterialButton _createButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         //Generic instructions
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_promotion_form);
+    }
+
+    public EditText getPromotionAcronymEditText()
+    {
+        return _promotionAcronymEditText;
+    }
+
+    public void setPromotionAcronymEditText(EditText promotionAcronymEditText)
+    {
+        _promotionAcronymEditText = promotionAcronymEditText;
+    }
+
+    public EditText getPromotionEntitledEditText()
+    {
+        return _promotionEntitledEditText;
+    }
+
+    public void setPromotionEntitledEditText(EditText promotionEntitledEditText)
+    {
+        _promotionEntitledEditText = promotionEntitledEditText;
+    }
+
+    public MaterialButton getCreateButton()
+    {
+        return _createButton;
+    }
+
+    public void setCreateButton(MaterialButton createButton)
+    {
+        _createButton = createButton;
     }
 }
