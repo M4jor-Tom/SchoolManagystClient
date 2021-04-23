0e0737ae5cd9aa5e7b7a3ac4a8a2c51a44093046
Tom <tom-vautray@hotmail.fr>
2021-04-23 09:15:47 +0200

    Added "Add promotion" button to promotions activity

--- app/src/main/java/com/example/schoolmanagystclient/PromotionActivity.java
+++ app/src/main/java/com/example/schoolmanagystclient/PromotionActivity.java
@@ -5,16 +5,21 @@ import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.button.MaterialButton;
+
 public class PromotionActivity extends AppCompatActivity
 {
     private ListView _promotionsListView;
+    private MaterialButton _promotionFormButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_promotions);
+
         setPromotionsListView((ListView)findViewById(R.id.promotionsListView));
+        setPromotionFormButton((MaterialButton)findViewById(R.id.promotionFormButton));
     }
 
     public ListView getPromotionsListView()
@@ -25,5 +30,15 @@ public class PromotionActivity extends AppCompatActivity
     public void setPromotionsListView(ListView promotionsListView)
     {
         _promotionsListView = promotionsListView;
+    }
+
+    public MaterialButton getPromotionFormButton()
+    {
+        return _promotionFormButton;
+    }
+
+    public void setPromotionFormButton(MaterialButton promotionFormButton)
+    {
+        _promotionFormButton = promotionFormButton;
     }
 }
