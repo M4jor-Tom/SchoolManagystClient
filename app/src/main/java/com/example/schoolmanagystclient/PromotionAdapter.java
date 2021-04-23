2121163d56ae57e43f148e14634eb76610e34ed3
Tom <tom-vautray@hotmail.fr>
2021-04-23 16:21:28 +0200

    Added class PromotionAdapter extends MyAdapter<Promotion>

---
+++ app/src/main/java/com/example/schoolmanagystclient/PromotionAdapter.java
@@ -0,0 +1,48 @@
+package com.example.schoolmanagystclient;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class PromotionAdapter extends MyAdapter<Promotion>
+{
+    private static final String TAG = "PromotionAdapter";
+
+    public PromotionAdapter(Context context, List<Promotion> promotions)
+    {
+        super(context, promotions);
+    }
+
+    @Override
+    public long getItemId(int position)
+    {
+        return getEntities().get(position).getId();
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent)
+    {
+        //Loading view
+        View view = selectView(convertView, R.layout.layout_promotion_option);
+
+        //Getting student
+        Promotion promotion = getEntities().get(position);
+
+        //Getting TextViews in layout
+        TextView acronym = (TextView)view.findViewById(R.id.promotionAcronymTextViewOption);
+        TextView entitled = (TextView)view.findViewById(R.id.promotionEntitledTextViewOption);
+
+        //Filling layout
+        acronym.setText(promotion.getAcronym());
+        entitled.setText(promotion.getEntitled());
+
+        //Printing selection
+        Log.i(TAG, promotion + " selected");
+
+        return view;
+    }
+}
