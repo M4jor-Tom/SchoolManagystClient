650332529ccaeb22c10dd5693b7bc2333fc4a783
Tom <tom-vautray@hotmail.fr>
2021-04-23 09:21:44 +0200

    Added "Add student" button to student activity

--- app/src/main/java/com/example/schoolmanagystclient/StudentActivity.java
+++ app/src/main/java/com/example/schoolmanagystclient/StudentActivity.java
@@ -5,9 +5,12 @@ import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.button.MaterialButton;
+
 public class StudentActivity extends AppCompatActivity
 {
     private ListView _studentsListView;
+    private MaterialButton _studentFormButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -16,6 +19,7 @@ public class StudentActivity extends AppCompatActivity
         setContentView(R.layout.activity_students);
 
         setStudentsListView((ListView)findViewById(R.id.studentsListView));
+        setStudentFormButton((MaterialButton)findViewById(R.id.studentFormButton));
     }
 
     public ListView getStudentsListView()
@@ -26,5 +30,15 @@ public class StudentActivity extends AppCompatActivity
     public void setStudentsListView(ListView studentsListView)
     {
         _studentsListView = studentsListView;
+    }
+
+    public MaterialButton getStudentFormButton()
+    {
+        return _studentFormButton;
+    }
+
+    public void setStudentFormButton(MaterialButton studentFormButton)
+    {
+        _studentFormButton = studentFormButton;
     }
 }
