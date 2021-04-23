2b08c87aa69a438b7d20e214e23ecfa1ff9bc704
Tom <tom-vautray@hotmail.fr>
2021-04-23 15:14:22 +0200

    Added class MyAdapter<Entity>

---
+++ app/src/main/java/com/example/schoolmanagystclient/MyAdapter.java
@@ -0,0 +1,54 @@
+package com.example.schoolmanagystclient;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.BaseAdapter;
+
+import java.util.List;
+
+abstract public class MyAdapter<Entity> extends BaseAdapter
+{
+    private Context _context;
+    private List<Entity> _entities;
+
+    public MyAdapter(Context context, List<Entity> entities)
+    {
+        setContext(context);
+        setEntities(entities);
+    }
+
+    //Use this method in getView()
+    protected View selectView(View convertView, int layoutId)
+    {
+        if(convertView == null)
+            return getLayoutInflater().inflate(layoutId, null);
+
+        return convertView;
+    }
+
+    public Context getContext()
+    {
+        return _context;
+    }
+
+    public void setContext(Context context)
+    {
+        _context = context;
+    }
+
+    public List<Entity> getEntities()
+    {
+        return _entities;
+    }
+
+    public void setEntities(List<Entity> entities)
+    {
+        _entities = entities;
+    }
+
+    public LayoutInflater getLayoutInflater()
+    {
+        return LayoutInflater.from(getContext());
+    }
+}
