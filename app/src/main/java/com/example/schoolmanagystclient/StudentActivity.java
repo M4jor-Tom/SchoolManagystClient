aa5c5247b19e255025e5cbb57dedd3336b4d3abe
Tom <tom-vautray@hotmail.fr>
2021-04-23 09:09:07 +0200

    Added studentsListView to students activity

--- app/src/main/java/com/example/schoolmanagystclient/StudentActivity.java
+++ app/src/main/java/com/example/schoolmanagystclient/StudentActivity.java
@@ -1,15 +1,30 @@
 package com.example.schoolmanagystclient;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class StudentActivity extends AppCompatActivity
 {
+    ListView _studentsListView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_students);
+
+        setStudentsListView((ListView)findViewById(R.id.studentsListView));
+    }
+
+    public ListView getStudentsListView()
+    {
+        return _studentsListView;
+    }
+
+    public void setStudentsListView(ListView studentsListView)
+    {
+        _studentsListView = studentsListView;
     }
 }
