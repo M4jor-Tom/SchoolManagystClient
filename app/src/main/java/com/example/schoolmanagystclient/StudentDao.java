b6eb6618df69799f11350512647d39fd7c2f2ee1
Tom <tom-vautray@hotmail.fr>
2021-04-28 17:54:32 +0200

    Created StudentDao

---
+++ app/src/main/java/com/example/schoolmanagystclient/StudentDao.java
@@ -0,0 +1,27 @@
+package com.example.schoolmanagystclient;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface StudentDao
+{
+    @Query("SELECT * FROM student")
+    public List<Promotion> getAllStudents();
+
+    //@Query("SELECT * FROM student WHERE promotionId = :promotionId")
+    //public Promotion getStudentByPromotionId(long promotionId);
+
+    @Query("SELECT * FROM student WHERE _id = :studentId")
+    public Promotion getStudentById(long studentId);
+
+    @Insert
+    void insertAll(Student... students);
+
+    @Delete
+    void delete(Student student);
+}
